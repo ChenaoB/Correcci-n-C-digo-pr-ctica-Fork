f90028a1ef3afb71f7d0244adc6c58b5e389e8b5
Cesar Osorio <cesaral95@hotmail.com>
2025-07-29 11:41:36 -0500

    Modificado Programa_Corregir.py

--- Programa_Corregir.py
+++ Programa_Corregir.py
@@ -1,28 +1,30 @@
-# Función para calcular el volumen de un cilindro
-def volumen_cilindro(radio, altura):
+def vol_cilindro(radio, altura):
     return 3.14 * radio * radio * altura
 
-# Función para calcular el volumen de un prisma rectangular
-def volumen_prisma_rectangular(largo, ancho, altura):
+
+def vol_prisma(largo, ancho, altura):
     return largo * ancho * altura
 
-# Función para calcular el volumen de un cubo
-def volumen_cubo(lado):
+
+def vol_cubo(lado):
     return lado ** 3
 
-# Función principal que ejecuta los cálculos
-def calcular_volumenes():
+
+def calcular():
     radio = 5
-    altura_cilindro = 10
-    print('El volumen del cilindro es:', volumen_cilindro(radio, altura_cilindro))
+    altura_cil = 10
+    print('El volumen del cilindro es:', vol_cilindro(radio, altura_cil))
 
     largo = 6
     ancho = 4
     altura_prisma = 2
-    print('El volumen del prisma rectangular es:', volumen_prisma_rectangular(largo, ancho, altura_prisma))
+    print('El volumen del prisma rectangular es:', vol_prisma(largo, ancho, altura_prisma))
 
     lado_cubo = 3
-    print('El volumen del cubo es:', volumen_cubo(lado_cubo))
+    print('El volumen del cubo es:', vol_cubo(lado_cubo))
 
-# Ejecutar cálculos
-calcular_volumenes()
+
+calcular()
+
+
+
